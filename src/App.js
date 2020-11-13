517f837368e9cc3f66be34e61094326f09fdb7de
Harshit Sahu <sahu_harshit@yahoo.in>
2020-11-13 14:48:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import LoginButton from './components/Login';
 import Profile from './components/Profile';
+import LogoutButton from './components/Logout';
 
 function App() {
   return (
    <>
     <LoginButton />
     <Profile />
+    <LogoutButton />
    </>
   );
 }
