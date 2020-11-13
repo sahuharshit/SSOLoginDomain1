ac479c3282a472a8b73d25bce322fd0a0705fcc3
Harshit Sahu <sahu_harshit@yahoo.in>
2020-11-13 14:39:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginButton from './components/Login';
+
+function App() {
+  return (
+    <LoginButton />
+  );
+}
+
+export default App;
