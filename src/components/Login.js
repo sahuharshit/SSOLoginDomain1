e15af80806cace249f9ed482658dd42d76db5498
Harshit Sahu <sahu_harshit@yahoo.in>
2020-11-17 14:31:55 +0530

    login button implementation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+    const {loginWithRedirect} = useAuth0();
+    const {user, isAuthenticated, isLoading} = useAuth0();
+    return !isAuthenticated && <button onClick={
+        () => loginWithRedirect()
+    }>Log In</button>;
 };
 
 export default LoginButton;
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
 
 const LogoutButton = () => {
-  const { logout } = useAuth0();
+    const {logout} = useAuth0();
+    const {user, isAuthenticated, isLoading} = useAuth0();
 
-  return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
-      Log Out
+
+    return isAuthenticated && <button onClick={
+        () => logout({returnTo: window.location.origin})
+    }>
+        Log Out
     </button>
-  );
+
 };
 
 export default LogoutButton;
