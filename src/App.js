5b84f12bab8689bff77753851b01599b078f5ae5
Harshit Sahu <sahu_harshit@yahoo.in>
2020-11-17 18:00:32 +0530

    comment 4

--- src/App.js
+++ src/App.js
@@ -5,13 +5,38 @@ import Profile from './components/Profile';
 import LogoutButton from './components/Logout';
 
 function App() {
-  return (
-   <>
-    <LoginButton />
-    <Profile />
-    <LogoutButton />
-   </>
-  );
+    return (
+        <>
+            <LoginButton/>
+            <Profile/>
+            <br/><br/><br/>
+            <a href="https://secondappsso.vercel.app/"
+                style={
+                    {
+                        padding: 30,
+                        border: '1px solid',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: 400,
+                        margin: 'auto'
+                    }
+            }>Summary Maker</a>
+            <br/><br/><br/>
+            <a href="https://secondappsso.vercel.app/"
+                style={
+                    {
+                        padding: 30,
+                        border: '1px solid',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: 400,
+                        margin: 'auto'
+                    }
+            }>Presentation Maker</a>
+
+            <LogoutButton/>
+        </>
+    );
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,9 +4,12 @@ import {useAuth0} from "@auth0/auth0-react";
 const LoginButton = () => {
     const {loginWithRedirect} = useAuth0();
     const {user, isAuthenticated, isLoading} = useAuth0();
-    return !isAuthenticated && <button onClick={
+    return !isAuthenticated && <>
+    <br/><br/><br/>
+      <button onClick={
         () => loginWithRedirect()
-    }>Log In</button>;
+    } style={{padding: 20, display:'flex', justifyContent:'center', margin:'auto'}}>Log In</button>
+      </>
 };
 
 export default LoginButton;
